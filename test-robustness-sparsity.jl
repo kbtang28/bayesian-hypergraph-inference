e8e8f204708d2fe211ad0254437389690d2df1c8
Katerina Tang <kbt28@cornell.edu>
2026-05-01 14:17:54 -0400

    add script for sparsity experiment

---
+++ test-robustness-sparsity.jl
@@ -0,0 +1,141 @@
+using Distributed
+
+num_procs = parse(Int, ENV["SLURM_NTASKS"])
+addprocs(max(num_procs-1, 0), topology=:master_worker)
+
+# activate environment
+@everywhere begin
+    import Pkg
+    Pkg.activate(@__DIR__)
+    Pkg.instantiate()
+end
+
+# load dependencies...
+@everywhere begin
+    using Random, Printf, SparseBayes, LinearAlgebra, Dates, DelimitedFiles, ProgressMeter
+
+    include("gen-rand-hyperg.jl")
+    include("hyperg-kuramoto.jl")
+    include("this-og.jl")
+    include("this-bayes.jl")
+    include("performance-measures.jl")
+
+end
+
+# experiment & model parameters (master)
+n = 10
+t2_array = 0.05:0.05:0.75
+t3_array = 0.05:0.05:0.75
+
+ooi = [2,3]
+dmax = 2
+
+N = 250
+σ = 0.2
+n_itr = 80
+λs = [0.01, 0.05, 0.1, 0.5, 1.0]
+
+# broadcast globals to all workers
+@everywhere begin
+    const n_const    = $n
+    const ooi_const  = $ooi
+    const dmax_const = $dmax
+    const λs_const   = $λs
+    const N_const    = $N
+    const σ_const    = $σ
+end
+
+# worker-side utility functions
+@everywhere begin
+    function _get_aurocs(Ainf, n, A2l, A3l)
+        aurocs = zeros(Float64, 3)
+
+        tpr, fpr = my_ROC(abs.(Ainf[2]), A2l, abs.(Ainf[3]), A3l, n; verbosity=0)
+        aurocs[1] = get_auc(tpr, fpr)
+
+        tpr2, fpr2 = my_ROC(abs.(Ainf[2]), A2l, n)
+        aurocs[2] = get_auc(tpr2, fpr2)
+
+        tpr3, fpr3 = my_ROC(abs.(Ainf[3]), A3l, n)
+        aurocs[3] = get_auc(tpr3, fpr3)
+
+        return aurocs
+    end
+
+    function _get_auprcs(Ainf, n, A2l, A3l)
+        auprcs = zeros(Float64, 3)
+
+        prec, rec = my_PRC(abs.(Ainf[2]), A2l, abs.(Ainf[3]), A3l, n; verbosity=0)
+        auprcs[1] = get_auc(prec, rec, rule="T")
+
+        prec2, rec2 = my_PRC(abs.(Ainf[2]), A2l, n)
+        auprcs[2] = get_auc(prec2, rec2, rule="T")
+
+        prec3, rec3 = my_PRC(abs.(Ainf[3]), A3l, n)
+        auprcs[3] = get_auc(prec3, rec3, rule="T")
+
+        return auprcs
+    end
+
+    # helper function to do inference and measure performance
+    function test_inference(t2, t3; n = n_const, ooi = ooi_const, dmax = dmax_const, λs = λs_const, N = N_const, σ = σ_const)        
+        # generate random hypergraph
+        A2, A3, A2l, A3l = gnm_random_hyperg(n, t2, t3)
+        p = (A2, A3, zeros(n), π/4, π/4)
+            
+        # sample data
+        X = rand(N, n) .- 0.5
+        Y = f_kuramoto_3rd(X, p...) .+ σ*randn(size(X))
+
+        # inference with Bayes-THIS & measure performance
+        opts = SBOpts(verbosity=0, nitr=2000, free_basis=[1])
+        ctrls = SBCtrlSettings(beta_update_frequency=3)
+        bayes_Ainf, _, _, _ = this_bayes(X, Y, ooi, dmax; opts=opts, ctrls=ctrls)
+            
+        bayes_aurocs = _get_aurocs(bayes_Ainf, n, A2l, A3l)
+        bayes_auprcs = _get_auprcs(bayes_Ainf, n, A2l, A3l)
+
+        # inference with THIS & measure performance (looping over λs...)
+        this_aurocs = zeros(Float64, 3, length(λs))
+        this_auprcs = zeros(Float64, 3, length(λs))
+        for (j, λ) in enumerate(λs)
+            this_Ainf, _, _ = this(X, Y, ooi, dmax, λ, 1e-4, 1000, with_scaling=true)
+
+            this_aurocs[:, j] = _get_aurocs(this_Ainf, n, A2l, A3l)
+            this_auprcs[:, j] = _get_auprcs(this_Ainf, n, A2l, A3l)
+        end
+        
+        return bayes_aurocs, this_aurocs, bayes_auprcs, this_auprcs
+    end
+end
+
+# ----------- RUN EXPERIMENT -----------
+timestamp = Dates.format(now(), "yyyy-mm-dd")
+
+# allocate storage for results
+bayes_aurocs = zeros(Float64, 3, n_itr, length(t2_array), length(t3_array))
+this_aurocs  = zeros(Float64, 3, length(λs), n_itr, length(t2_array), length(t3_array))
+bayes_auprcs = zeros(Float64, 3, n_itr, length(t2_array), length(t3_array))
+this_auprcs  = zeros(Float64, 3, length(λs), n_itr, length(t2_array), length(t3_array))
+
+for (j, t2) in enumerate(t2_array)
+    for (k, t3) in enumerate(t3_array)
+        results = @showprogress pmap(1:n_itr) do _
+            test_inference(t2, t3)
+        end
+
+        for i in 1:n_itr
+            bayes_aurocs[:, i, j, k]   .= results[i][1]
+            this_aurocs[:, :, i, j, k] .= results[i][2]
+            bayes_auprcs[:, i, j, k]   .= results[i][3]
+            this_auprcs[:, :, i, j, k] .= results[i][4]
+        end
+    end
+end
+
+writedlm("out/sparsity/bayes-aurocs-$(timestamp)-sparsity-2.txt", bayes_aurocs)
+writedlm("out/sparsity/this-aurocs-$(timestamp)-sparsity-2.txt", this_aurocs)
+writedlm("out/sparsity/bayes-auprcs-$(timestamp)-sparsity-2.txt", bayes_auprcs)
+writedlm("out/sparsity/this-auprcs-$(timestamp)-sparsity-2.txt", this_auprcs)
+
+rmprocs(workers())
